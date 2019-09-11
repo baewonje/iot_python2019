dd8af40e2b122a3507305c08125f9776872a410b
baeownje <you@example.com>
2019-09-11 17:28:16 +0900

    /11

---
+++ 3_bigdata/01_Collection/02_OpenAPI/02_data/get_weather_json.py
@@ -0,0 +1,83 @@
+import urllib.request
+import datetime
+import json
+import time
+import csv
+output_file = '대구광역시_동구_단기예보5.csv'
+
+access_key = 'pUzCOfH0tRU0daYQ47fcsGrDUtqRi9%2F5tRBwHn980XHUpP8C4oWuyh4JK2DIjgoDqzn%2FGS51KnhRrXZVEdrWfg%3D%3D'
+x_coodinate = "89"
+y_coodinate = "91"
+
+csv_out_file = open(output_file, 'w', newline='')
+filewriter = csv.writer(csv_out_file)
+header_list = ['baseDate','baseTime','category','fcstDate','fcstTime','fcstValue','nx','ny']
+filewriter.writerow(header_list)
+
+def get_Requrest_URL(url):
+    req = urllib.request.Request(url)
+
+    try:
+        response = urllib.request.urlopen(req)
+        if response.getcode() == 200:
+            print("[%s] Url:%s => Request Success" % (datetime.datetime.now(), url))
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL: %s" % (datetime.datetime.now(), url))
+        return None
+
+def get_Weather_URL(day_time):
+    base_date = time.strftime('%Y%m%d', time.localtime(time.time()))
+    end_point = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastTimeData"
+    parameters = ("?base_date=%s&base_time=%s&nx=%s&ny=%s&_type=json&numOfRows=100&serviceKey=%s") \
+                 %(base_date,day_time,x_coodinate,y_coodinate,access_key)
+
+    url = end_point + parameters
+    retData = get_Requrest_URL(url)
+    if (retData == None):
+        return None
+    else:
+        return json.loads(retData)
+
+def Make_Weather_Json(day_time):
+    jsonData = get_Weather_URL(day_time)
+    json_weather_result = []
+    for data in jsonData['response']['body']['items']['item']:
+        baseDate = data["baseDate"]
+        baseTime =data["baseTime"]
+        category = data["category"]
+        fcstDate = data["fcstDate"]
+        fcstTime = data["fcstTime"]
+        fcstValue = data["fcstValue"]
+        nx = data["nx"]
+        ny = data["ny"]
+
+        row_list = [baseDate,baseTime,category,fcstDate,fcstTime,fcstValue,nx,ny]
+        filewriter.writerow(row_list)
+        # json_weather_result.append({baseDate,baseTime,category,fcstDate,fcstTime,fcstValue,nx,ny})
+
+
+    yyyymmdd = time.strftime('%Y%m%d', time.localtime(time.time()))
+    # row_list_output = []
+    # for row_list in json_weather_result:
+    #     row_list_output.append(row_list.values())
+    #     filewriter.writerow(row_list_output)
+    #     print(row_list)
+
+    # with open('동구_신암동_초단기예보조회_%s%s.json' % (yyyymmdd, day_time), 'w', encoding='utf-8') as outfile:
+    #     retJson = json.dumps(json_weather_result, indent=4, sort_keys=True, ensure_ascii=False)
+    #     outfile.write(retJson)
+    print('동구_신암동_초단기예보조회_%s%s.json' % (yyyymmdd, day_time))
+
+def get_Realtime_Weather_Info():
+    if time.localtime().tm_min in range(0,31):
+        real_time = str(time.localtime().tm_hour - 1) + '59'
+    else:
+        real_time = time.strftime('%H%M', time.localtime(time.time()))
+    # real_time = time.strftime('%H%M', time.localtime(time.time()))
+    if len(real_time) == 3:
+        real_time = '0' + real_time
+    Make_Weather_Json(real_time)
+
+get_Realtime_Weather_Info()
--- 3_bigdata/01_Collection/02_OpenAPI/02_data/q3.py
+++ 3_bigdata/01_Collection/02_OpenAPI/02_data/q3.py
@@ -22,6 +22,8 @@ def get_Request_URL(url):
         return None
 
 def get_Weather_URL(yyyymmdd,day_time):
+    x_coodinate = "89"
+    y_coodinate = "91"
     # (1)기상 정보(동네예보정보 조회 서비스) request 보내기 전, url 만드는 함수
     end_point = "http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastTimeData"  # ForecastTimeData 서비스를 이용할 것
 
@@ -39,10 +41,21 @@ def get_Weather_URL(yyyymmdd,day_time):
     else:
         return json.loads(retData)
 
-def Make_Weather_Json(day_time):
+def get_Realtime_Weather_Info(): #시간 정보를 보정하여 Make_Weather_Json() 을 호출한다.
+    if int(time.strftime('%M')) < 30 :
+        an_hour_later =str(int(datetime.datetime.today().hour)-1)
+        day_time = an_hour_later
+        return day_time
+    else:
+        now_time=time.strftime('%H%M', time.localtime(time.time()))
+        day_time = now_time
+        return day_time
+
+def main():
+    json_weather_result = []
     # (1) 기상 정보(동네예보정보 조회서비스) json 파일을 생성하는 함수
     yyyymmdd=time.strftime('%Y%m%d',time.localtime(time.time()))
-    jsonData = get_Weather_URL(yyyymmdd,day_time)
+    jsonData = get_Weather_URL(yyyymmdd,get_Realtime_Weather_Info)
     index=0
     for i in jsonData['response']['body']['items']['item']:
         # jsonData를 parsing 하여 json_weather_result에 저장된 예제 샘플을 참고하여 데이터를 저장한다.
@@ -65,17 +78,9 @@ def Make_Weather_Json(day_time):
 
     print('동구_신암동_초단기예보조회_%s_%s.json SAVED'%(ymd,day_time))
 
-def get_Realtime_Weather_Info(): #시간 정보를 보정하여 Make_Weather_Json() 을 호출한다.
-    if int(time.strftime('%M')) < 30 :
-        an_hour_later =str(int(datetime.datetime.today().hour)-1)
-        Make_Weather_Json(an_hour_later)
-    else:
-        now_time=time.strftime('%H%M', time.localtime(time.time()))
-        Make_Weather_Json(now_time)
-json_weather_result = []
-# 동구 신암동의 좌표를 세팅한다.
-x_coodinate = "89"
-y_coodinate = "91"
+
+if __name__ == '__main__':
+    main()
 
 
-get_Realtime_Weather_Info()
+
